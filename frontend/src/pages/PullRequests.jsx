cd8b2299986ca7ebce388a139e05c9da4caad7b2
Khushi <khushichauhan2506@gmail.com>
2026-05-15 11:18:22 +0530

    pull request analysis

---
+++ frontend/src/pages/PullRequests.jsx
@@ -0,0 +1,9 @@
+const PullRequests = () => {
+  return (
+    <div className="text-white text-4xl font-bold">
+      Pull Requests
+    </div>
+  )
+}
+
+export default PullRequests
---
+++ frontend/src/pages/Vulnerabilities.jsx
@@ -0,0 +1,9 @@
+const Vulnerabilities = () => {
+  return (
+    <div className="text-white text-4xl font-bold">
+      Vulnerabilities
+    </div>
+  )
+}
+
+export default Vulnerabilities
